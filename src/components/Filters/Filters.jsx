c925228a90b804072d0df82ce90dad3813f89f70
Ivan <Vangold915@gmail.com>
2024-05-22 16:15:03 +0700

    Filters component is filled

---
+++ src/components/Filters/Filters.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setFilter } from '../../store';
+
+const Filters = () => {
+  // Получаем диспетчер
+  const dispatch = useDispatch()
+  // Получаем текущее значение фильтра
+  const status = useSelector((state) => state.filter)
+
+  return (
+    <div className='col-3'>
+      <h3>Filters</h3>
+      {['all', 'active', 'completed'].map(filter => (
+        <div key={filter} className='form-check' style={{textAlign: 'left'}}>
+          <input 
+            id={filter}
+            type="radio"
+            checked={filter === status}
+            onChange={() => dispatch(setFilter)}
+            className='form-check-input'
+          />
+          <label htmlFor={filter} className='form-check-label'>
+            {filter.toUpperCase()}
+          </label>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Filters;
