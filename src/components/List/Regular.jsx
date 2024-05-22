b5d60b30e6879176128f6397782baa25f783ff4a
Ivan <Vangold915@gmail.com>
2024-05-22 16:49:49 +0700

    Regilar List Component is filled

---
+++ src/components/List/Regular.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { removeTodo, updateTodo } from '../../store';
+
+const Regular = ({ todo }) => {
+  // Получаем диспетчер
+  const dispatch = useDispatch()
+  // Получаем все свойства задачи
+  const {id, text, done, edit} = todo
+
+  return (
+    <li className='list-group-item d-flex align-items-center'>
+      <input 
+        className='form-check-input'
+        type="checkbox" 
+        checked={done}
+        onChange={() => dispatch(updateTodo({ id, changes: { done: !done } }))}
+      />
+      <p
+        className={`flex-grow-1 m-0 ${
+          done ? 'text-muted text-decortion-line-through' : ''
+        }`}
+      >
+        { text }
+      </p>
+      <button
+        onClick={() => dispatch(updateTodo({ id, changes: { edit: !edit } }))}
+        className='btn btn-outline-info'
+        disabled={done}
+      >
+        <i className='bi bi-pencil'></i>
+      </button>
+      <button
+        onClick={() => dispatch(removeTodo(id))}
+        className='btn btn-outline-danger'
+      >
+        <i className="bi bi-trash"></i>
+      </button>
+    </li>
+  );
+}
+
+export default Regular;
